5e9b5b46e98926734181df1b5cf4a599aed96ba5
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-04-30 10:59:19 -0300

    added subplots

--- bank_stock_data.py
+++ bank_stock_data.py
@@ -118,9 +118,84 @@ plt.title("A Histogram of Daily Closing Stock Prices for the 5 Largest Banks in 
 plt.ylabel("Observations", fontsize = 20)
 plt.xlabel("Stock Prices", fontsize = 20)
 
-########################
+################################################
+################################################
 #Create subplots in Python
+################################################
+################################################
+
 ########################
+#Subplot 1
+########################
+plt.subplot(2,2,1)
 
+#Generate the boxplot
+plt.boxplot(bank_data.transpose())
 
-#Push the file to AWS S3
+#Add titles to the chart and axes
+plt.title('Boxplot of Bank Stock Prices (5Y Lookback)')
+plt.xlabel('Bank', fontsize = 20)
+plt.ylabel('Stock Prices')
+
+#Add labels to each individual boxplot on the canvas
+ticks = range(1, len(bank_data.columns)+1)
+labels = list(bank_data.columns)
+plt.xticks(ticks,labels)
+
+########################
+#Subplot 2
+########################
+plt.subplot(2,2,2)
+
+#Create the x-axis data
+dates = bank_data.index.to_series()
+dates = [pd.to_datetime(d) for d in dates]
+
+#Create the y-axis data
+WFC_stock_prices =  bank_data['WFC']
+
+#Generate the scatterplot
+plt.scatter(dates, WFC_stock_prices)
+
+#Add titles to the chart and axes
+plt.title("Wells Fargo Stock Price (5Y Lookback)")
+plt.ylabel("Stock Price")
+plt.xlabel("Date")
+
+########################
+#Subplot 3
+########################
+plt.subplot(2,2,3)
+
+#Create the x-axis data
+dates = bank_data.index.to_series()
+dates = [pd.to_datetime(d) for d in dates]
+
+#Create the y-axis data
+BAC_stock_prices =  bank_data['BAC']
+
+#Generate the scatterplot
+plt.scatter(dates, BAC_stock_prices)
+
+#Add titles to the chart and axes
+plt.title("Bank of America Stock Price (5Y Lookback)")
+plt.ylabel("Stock Price")
+plt.xlabel("Date")
+
+########################
+#Subplot 4
+########################
+plt.subplot(2,2,4)
+
+#Generate the histogram
+plt.hist(bank_data.transpose(), bins = 50)
+
+#Add a legend to the histogram
+plt.legend(bank_data.columns,fontsize=20)
+
+#Add titles to the chart and axes
+plt.title("A Histogram of Daily Closing Stock Prices for the 5 Largest Banks in the US (5Y Lookback)")
+plt.ylabel("Observations")
+plt.xlabel("Stock Prices")
+
+plt.tight_layout()
